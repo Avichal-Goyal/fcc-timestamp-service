2a875a5543269eafe2e89000c9d0a18d3e824d96
Avichal-Goyal <avichalgoyal2105@gmail.com>
2025-07-22 17:11:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get("/api/:date_string", (req, res) => {
+  const dateString = req.params.date_string;
+  let date;
+  date = isNaN(dateString) ? new Date(dateString) : new Date(parseInt(dateString));
+  if(date.toString() === 'Invalid Date') {
+    return res.json({error: 'Invalid Date'});
+  }
+  let unixDate = date.getTime();
+  let utcDate = date.toUTCString();
+
+  res.json({
+    unix: unixDate,
+    utc: utcDate
+  });
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
